ef12779643a8ca7c2acbb5706406095b474d9a5c
GabeenKim <rlarkqls5551250@gmail.com>
2022-05-12 09:32:05 +0900

    과제작성

---
+++ src/api/data.js
@@ -0,0 +1,20 @@
+
+let data = [
+    {
+        id :1,
+        content : 'Avengers', 
+        writer : 1
+    },
+    {
+        id :2,
+        content : 'Spider-man',
+        writer : 2
+    },
+    {
+        id :3,
+        content : 'Ant-man',
+        writer : 3
+    }
+];
+
+export{ data};
